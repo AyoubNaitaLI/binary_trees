38f7de4e50a851ea23c9bf1ce696ef28d847f1e4
AyoubNaitaLI <naitaliayoub4@gmail.com>
2024-02-28 13:37:44 +0000

    commit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,16 +8,6 @@
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-/**
  * binary_tree_node - Creates a new binary tree node
  *
  * @parent: Pointer to the parent node
